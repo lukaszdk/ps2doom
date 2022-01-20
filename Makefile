d59157438dbd342831ad95a4e9765bce46ee95e0
wolfysdl <andregui17@outlook.com>
2022-01-20 07:27:56 -0300

    Add nosound command line support

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 
-EE_OBJS = am_map.o cosmito_wav.c elf_structure.c d_items.o d_main.o d_net.o doomdef.o doomstat.o \
+EE_OBJS = am_map.o cosmito_wav.c elf_structure.c d_command.c  d_items.o d_main.o d_net.o doomdef.o doomstat.o \
 dstrings.o f_finale.o f_wipe.o g_game.o hu_lib.o hu_stuff.o  \
 i_main.o i_net.o i_sound.o i_system.o i_video.o info.o m_argv.o m_bbox.o \
 m_cheat.o m_fixed.o m_menu.o m_misc.o m_random.o m_swap.o mixer_thread.o mixer.o mmus2mid.o p_ceilng.o \
---
+++ d_command.c
@@ -0,0 +1,70 @@
+#include <string.h>
+
+#include "include/d_main.h"
+
+/****************************************************************************
+** ps2doom cheat support - code typing and command line
+*****************************************************************************/
+void ps2_do_cheat(int cheat)
+{
+    event_t event;
+    char *str;
+    int i;
+
+    switch (cheat)
+    {
+           case 1: // God Mode
+	   str = "iddqd";		
+	   break;		
+           case 2: // Good fucking arsenal
+	   str = "idfa";		
+	   break;
+	   case 3: // Key Full Ammo	
+	   str = "idkfa";		
+	   break;
+	   case 4: // No Clipping		str = "idclip";		
+	   break;
+	   case 5: // Toggle Map         
+	   str = "iddt";		
+	   break;
+	   case 6: // Invincible with Chainsaw 	         
+	   str = "idchoppers";
+	   break;
+	   case 7:  // Berserker Strength Power-up    	 
+	   str = "idbeholds";		
+	   break;
+	   case 8:  //Invincibility Power-up
+	   str = "idbeholdv";		
+	   break;		
+	   case 9:  // Invisibility Power-Up
+	   str = "idbeholdi";		
+	   break;
+           case 10: //Automap Power-up      
+	   str = "idbeholda";		
+	   break;
+	   case 11: //Anti-Radiation Suit Power-up     
+	   str = "idbeholdr";
+	   break;
+	   case 12: // Light-Amplification Visor Power-up
+	   str = "idbeholdl";
+	   break;
+	   case 13: 
+	   str = "no_sound";
+	   SjPCM_Pause();
+	   default:
+	   return;
+
+       }
+
+    for (i=0; i<strlen(str); i++) 
+    { 
+      event.type = ev_keydown; 
+      event.data1 = str[i]; 
+      D_PostEvent (&event); 
+      event.type = ev_keyup; 
+      event.data1 = str[i]; 
+      D_PostEvent (&event);	
+    }
+}
+
+
