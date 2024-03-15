873f9d932a4a893eb6cc444a724f9f4e3875d9e5
Adilson Enio Pierog <dirsors@gmail.com>
2024-03-15 20:26:36 -0300

    Update i_system.c

--- i_system.c
+++ i_system.c
@@ -108,16 +108,16 @@ int  I_GetTime (void)
 //
 void I_Init (void)
 {
-    if ( SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO/*|SDL_INIT_JOYSTICK*/) < 0 )
+    if ( SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO|SDL_INIT_JOYSTICK) < 0 )
         I_Error("Could not initialize SDL: %s", SDL_GetError());
 
     I_InitSound();
     //  I_InitGraphics();
-#if 0
+
 	SDL_Joystick *joystick;
 
 	joystick = SDL_JoystickOpen(0);
-#endif
+
 }
 
 //
